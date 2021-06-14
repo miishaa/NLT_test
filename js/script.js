5b6fa7dd71e65a2d8c5176630eca899ea0eea12c
miishaa <supermikhailo@gmail.com>
2021-06-14 08:53:23 -0700

    respomsive

--- js/script.js
+++ js/script.js
@@ -2,5 +2,35 @@ $('.slider').slick({
     arrows:true,
     slidesToShow:4,
     slidesToScroll:1,
-    // variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 770,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+$('.header__burger').click(function (event) {
+    $('.header__burger,.header__navigation').toggleClass('active');
+    $('body').toggleClass('lock');
 });
