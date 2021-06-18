860d07e47c10f141895eb92ea7700f062bb0adab
miishaa <supermikhailo@gmail.com>
2021-06-18 08:05:09 -0700

    firs version of casscade mobile menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+//slick-slider settings
+
 $('.slider').slick({
     arrows:true,
     slidesToShow:4,
@@ -30,8 +33,10 @@ $('.slider').slick({
     ]
 });
 
+//burger menu
+
 $('.header__burger').click(function (event) {
-    $('.header__burger,.header__navigation').toggleClass('active');
+    $('.header__burger,.header__navigation,.call-us').toggleClass('active');
     $('body').toggleClass('lock');
 });
 
@@ -216,6 +221,34 @@ function checkParams2() {
 }
 
 
+// check if it touch screen or not
+
+let isMobile = {
+    Android: function() {return navigator.userAgent.match(/Android/i);},
+    BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+    iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+    Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+    Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+    any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+let bodyCheck=document.querySelector('body');
+if(isMobile.any()){
+    bodyCheck.classList.add('touch');
+    let arrow=document.querySelectorAll('.arrow');
+    for(i=0; i<arrow.length; i++){
+        let thisLink=arrow[i].previousElementSibling;
+        let subMenu=arrow[i].nextElementSibling;
+        let thisArrow=arrow[i];
+
+        thisLink.classList.add('parent');
+        arrow[i].addEventListener('click', function(){
+            subMenu.classList.toggle('open');
+            thisArrow.classList.toggle('active');
+        });
+    }
+}else{
+    bodyCheck.classList.add('mouse');
+}
 
 
 
